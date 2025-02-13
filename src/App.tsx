24066fed0b6eb2b389c4f63a5e5c40a64100c7d0
varsha082004 <22bk1a1211@stpetershyd.com>
2025-02-13 15:12:18 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import StuResult from './stu-components/StuResult'
 import ShowMovies from './stu-components/ShowMovies'
+import MoviesDataFromURL from './stu-components/MoviesDataFromUrl'
 export default function App() {
   return (
     <div>
 
-    <ShowMovies/>
+    <MoviesDataFromURL/>
       
     </div>
   )
